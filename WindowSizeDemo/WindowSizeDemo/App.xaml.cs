03188638bf983466c7f9211cd1b944b5faaec881
David Britch <davidbritch@users.noreply.github.com>
2022-10-11 10:01:26 +0100

    Window position/sizing.

---
+++ WindowSizeDemo/WindowSizeDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace WindowSizeDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+	//protected override Window CreateWindow(IActivationState activationState) =>
+	//	new Window(new AppShell())
+	//	{
+	//		Width = 700,
+	//		Height = 500,
+	//		X = 100,
+	//		Y = 100
+	//	};
+}
---
+++ WindowSizeDemo/WindowSizeDemo/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowSizeDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+	}
+
+	void OnNewWindowClicked(object sender, EventArgs e)
+	{
+        try
+        {
+            Application.Current.OpenWindow(new Window(new MainPage()));
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+        }
+
+    }
+
+    void OnCloseWindowClicked(object sender, EventArgs e)
+    {
+        Application.Current.CloseWindow(Window);
+    }
+
+    void OnSetMinimumSizeClicked(object sender, EventArgs e)
+    {
+        Window.MinimumWidth = 640;
+        Window.MinimumHeight = 480;
+    }
+
+    void OnSetMaximumSizeClicked(object sender, EventArgs e)
+    {
+        Window.MaximumWidth = 800;
+        Window.MaximumHeight = 600;
+    }
+
+    void OnSetFreeSizeClicked(object sender, EventArgs e)
+    {
+        Window.MaximumWidth = double.PositiveInfinity;
+        Window.MaximumHeight = double.PositiveInfinity;
+
+        Window.MinimumWidth = 0;
+        Window.MinimumHeight = 0;
+    }
+
+    void OnSetCustomSizeClicked(object sender, EventArgs e)
+    {
+#if MACCATALYST
+        Window.MinimumWidth = 700;
+        Window.MaximumWidth = 700;
+        Window.MinimumHeight = 500;
+        Window.MaximumHeight = 500;
+
+        // Give the Window time to resize
+        Dispatcher.Dispatch(() =>
+        {
+            Window.MinimumWidth = 0;
+            Window.MinimumHeight = 0;
+            Window.MaximumWidth = double.PositiveInfinity;
+            Window.MaximumHeight = double.PositiveInfinity;
+        });
+#elif WINDOWS
+        Window.Width = 700;
+        Window.Height = 500;
+#endif
+    }
+
+    void OnCenterWindowClicked(object sender, EventArgs e)
+    {
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+        Window.X = (displayInfo.Width / displayInfo.Density - Window.Width) / 2;
+        Window.Y = (displayInfo.Height / displayInfo.Density - Window.Height) / 2;
+    }
+}
