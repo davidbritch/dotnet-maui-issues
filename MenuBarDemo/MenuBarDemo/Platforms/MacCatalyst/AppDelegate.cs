a93f22e0c207a93771c394682ebea92d01b99ce8
David Britch <davidbritch@users.noreply.github.com>
2023-10-25 09:41:17 +0100

    MenuBar demo

---
+++ MenuBarDemo/MenuBarDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MenuBarDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
