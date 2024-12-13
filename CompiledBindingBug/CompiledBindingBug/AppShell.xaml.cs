8477f35fa6fcfa157a28cab369630cf55d2bad5a
David Britch <davidbritch@users.noreply.github.com>
2024-12-13 15:23:30 +0000

    Compiled binding issue.

---
+++ CompiledBindingBug/CompiledBindingBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CompiledBindingBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CompiledBindingBug/CompiledBindingBug/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CompiledBindingBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //label.SetBinding(Label.ScaleProperty, "Value", source: slider);
+            label.SetBinding(Label.ScaleProperty, Binding.Create(static (Slider s) => s.Value, source: slider));
+        }
+    }
+}
